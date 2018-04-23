8635f4dc64d89c8044729db7a6d37d4a23fb804b
sakulayi <zh812357151@qq.com>
2018-04-24 00:49:30 +0800

    11

--- Test/ClassTest.py
+++ Test/ClassTest.py
@@ -12,7 +12,7 @@ class Child(Parent):
     def __init__(self,name):
         Parent.__init__(self,name)
         self.childName+=name
-    def printChildTitile(self):#11
+    def printChildTitile(self):#11222
         print(self.childName)
 
 
